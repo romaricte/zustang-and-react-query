f50db1e59277cc5a5f0a52f15818f8c574d610de
Tene Romaric <tenrom142@gmail.com>
2024-08-22 13:36:50 +0100

    commit1

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import { getUsers } from './api/user';
+import { useUserStore } from './state/userStore';
+import React from 'react';
+
+export default function App() {
+  const { filters } = useUserStore();
+
+  const { data } = useQuery({
+    queryKey: ['users', filters],
+    queryFn: () => getUsers(filters),
+  });
+
+  return (
+    <div>
+      <FiltersComponent />
+      {data?.map((user) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
+    </div>
+  );
+}
+
+function FiltersComponent() {
+  const { setFilters } = useUserStore();
+
+  // Imagine some form inputs here
+
+  return null;
+}
